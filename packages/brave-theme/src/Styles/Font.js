5904397e6a45c62b5eb47c10ece9d63b3d2dc672
Alex Dawson <alexecdawson@gmail.com>
2021-09-08 16:29:49 +1000

    Create Font file

--- packages/brave-theme/src/Styles/Font.js
+++ packages/brave-theme/src/Styles/Font.js
@@ -3,9 +3,11 @@ import { styled } from "frontity";
 export const H1 = styled.h1`
   font-size: 64px;
   margin: 0;
+  font-weight: 600;
 `;
 
 export const FeaturedText = styled.p`
   font-size: 24px;
   line-height: 150%;
+  font-weight: 300;
 `;
