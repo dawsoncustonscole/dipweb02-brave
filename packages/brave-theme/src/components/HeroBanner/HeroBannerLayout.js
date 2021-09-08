2f9805775193f7532e14dc47ea466067ca886d02
Alex Dawson <alexecdawson@gmail.com>
2021-09-08 18:23:58 +1000

    Fix absolute position hiding content

--- packages/brave-theme/src/components/HeroBanner/HeroBannerLayout.js
+++ packages/brave-theme/src/components/HeroBanner/HeroBannerLayout.js
@@ -1,17 +1,23 @@
+import { styled } from "frontity";
 import React from "react";
 import BackgroundImage from "./BackgroundImage";
 import HeroBannerContent from "./HeroBannerContent";
 import Overlay from "./Overlay";
 
+const StyledDiv = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
 function HeroBannerLayout(props) {
   const { backgroundImageSrc, content } = props;
 
   return (
-    <>
+    <StyledDiv class="hero-banner_layout">
       <Overlay />
       <BackgroundImage backgroundImageSrc={backgroundImageSrc} />
       <HeroBannerContent content={content} />
-    </>
+    </StyledDiv>
   );
 }
 
--- packages/brave-theme/src/components/Navigation/Navigation.js
+++ packages/brave-theme/src/components/Navigation/Navigation.js
@@ -1,6 +1,8 @@
 import React from "react";
 import NavigationLayout from "./NavigationLayout";
 
+// TODO Implement nav hide on scroll
+
 function Navigation() {
   return <NavigationLayout />;
 }
