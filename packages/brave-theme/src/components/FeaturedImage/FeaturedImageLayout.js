def8fd8f530cf0108886b0930dbe6f2358a7fed5
Alex Dawson <alexecdawson@gmail.com>
2021-09-09 15:23:20 +1000

    Fix safari column-reverse bug

--- packages/brave-theme/src/components/FeaturedImage/FeaturedImageLayout.js
+++ packages/brave-theme/src/components/FeaturedImage/FeaturedImageLayout.js
@@ -27,6 +27,16 @@ const StyledContainer = styled.div`
   }
 `;
 
+// Hack to solve weird Safari column-reverse behaviour
+const StyledDiv = styled.div`
+  @media not all and (min-resolution: 0.001dpcm) {
+    @media (max-width: 425px) {
+      margin-top: -4rem;
+      margin-bottom: 4rem;
+    }
+  }
+`;
+
 function FeaturedImageLayout(props) {
   const { content, imageSrc } = props;
 
@@ -53,8 +63,8 @@ function FeaturedImageLayout(props) {
           ></div>
           {content.map(renderContent)}
         </div>
-        <div>
-          <figure
+        <StyledDiv>
+          <div
             style={{
               margin: 0,
               borderRadius: "0.5rem",
@@ -67,8 +77,8 @@ function FeaturedImageLayout(props) {
               src={imageSrc}
               style={{ objectFit: "cover", width: "100%", height: "100%" }}
             />
-          </figure>
-        </div>
+          </div>
+        </StyledDiv>
       </StyledContainer>
     </StyledWrapper>
   );
